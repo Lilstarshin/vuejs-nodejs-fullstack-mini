931cbf6d197a606a3b1f176eeea8cde622f1895b
lilstar <lilstar.shin@gmail.com>
2022-06-03 17:32:29 +0900

    express server create

---
+++ server/index.js
@@ -0,0 +1,14 @@
+// @ts-check
+const express = require('express')
+const app = express()
+const PORT = 3000
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('Success')
+})
+
+app.listen(PORT, 'localhost', () => {
+  console.log(`App listening at http://localhost:${PORT}`)
+})
